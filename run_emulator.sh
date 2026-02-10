a422b1885a066921e3c74e2df5235f475bef3d81
Frederic Marchais <fredmarc@amazon.com>
2026-02-10 16:20:20 +0000

    Add helper script for running emulator on DCV

---
+++ run_emulator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Videopac Emulator Launcher for DCV
+
+export DISPLAY=:0
+export SDL_RENDER_DRIVER=software
+export SDL_AUDIODRIVER=dummy  # Disable audio to avoid ALSA errors
+
+./build/videopac --bios "roms/Philips C52 BIOS (19xx)(Philips)(FR).bin" "$@"
